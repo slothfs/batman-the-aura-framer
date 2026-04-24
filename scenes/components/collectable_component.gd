e7f69d097a2cdae5e2c4fba3e0235f34260447f5
AK3 <mjlabishek123@gmail.com>
2026-04-24 16:38:47 +0545

    added a log drop

---
+++ scenes/components/collectable_component.gd
@@ -0,0 +1,10 @@
+class_name CollectableComponents
+extends Area2D
+
+@export var collectable_name: String
+
+
+func _on_body_entered(body: Node2D) -> void:
+	if body is Player:
+		print("Collected")
+		get_parent().queue_free()
