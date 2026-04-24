cb62a74b3a194d03c5006f75d7afefaeab71b5aa
AK3 <mjlabishek123@gmail.com>
2026-04-24 22:59:48 +0545

    added a distructable rock and also added a crosshair to make it easy to aim for object

--- rocks.gd
+++ rocks.gd
@@ -1,22 +1,38 @@
+class_name Rock
 extends Sprite2D
 
 @onready var hurt_component: HurtComponent = $HurtComponent
 @onready var damage_component: DamageComponent = $DamageComponent
 
-var stone_scene = preload("res://scenes/objects/rocks/rocks.tscn")
+const STONE_SCENE: PackedScene = preload("res://scenes/objects/rocks/stone.tscn")
 
 func _ready() -> void:
 	hurt_component.hurt.connect(on_hurt)
 	damage_component.max_damaged_reached.connect(on_max_damage_reached)
 	
 func on_hurt(hit_damage: int) -> void:
-		damage_component.apply_damage(hit_damage)
+	damage_component.apply_damage(hit_damage)
+	material.set_shader_parameter("shake_intensity" , 0.5)
+	await get_tree().create_timer(1.0).timeout
+	material.set_shader_parameter("shake_intensity" , 0.0)
 
-func  on_max_damage_reached() -> void:
-	call_deferred("add_stone_scene")
+func on_max_damage_reached() -> void:
+	call_deferred("_handle_destruction")
+
+func _handle_destruction() -> void:
+	var tile_layer: TileMapLayer = get_parent() as TileMapLayer
+	if tile_layer:
+		var cell: Vector2i = tile_layer.local_to_map(tile_layer.to_local(global_position))
+		tile_layer.erase_cell(cell)
+		spawn_stone(tile_layer)
+	else:
+		spawn_stone(get_parent())
 	queue_free()
-	
-func add_stone_scene() -> void:
-	var stone_instance = stone_scene.instantiate() as Node2D
+
+func spawn_stone(parent_node: Node) -> void:
+	if parent_node == null:
+		return
+
+	var stone_instance: Node2D = STONE_SCENE.instantiate() as Node2D
 	stone_instance.global_position = global_position
-	get_parent().add_child(stone_instance)
+	parent_node.add_child(stone_instance)
--- scenes/characters/player/player.gd
+++ scenes/characters/player/player.gd
@@ -4,3 +4,30 @@ extends CharacterBody2D
 
 @export var current_tool: DataTypes.Tools = DataTypes.Tools.None
 var player_direction: Vector2
+
+func _process(_delta: float) -> void:
+	queue_redraw()
+
+func _draw() -> void:
+	if current_tool == DataTypes.Tools.None:
+		return
+		
+	var target_pos: Vector2 = Vector2.ZERO
+	if player_direction == Vector2.UP:
+		target_pos = Vector2(5, -51)
+	elif player_direction == Vector2.RIGHT:
+		target_pos = Vector2(25, -6)
+	elif player_direction == Vector2.DOWN:
+		target_pos = Vector2(-5, -1)
+	elif player_direction == Vector2.LEFT:
+		target_pos = Vector2(-22, -6)
+	else:
+		target_pos = Vector2(-5, -1) # default down/front
+		
+	# Draw a translucent circle matching the hit collision area
+	draw_arc(target_pos, 6.0, 0, TAU, 16, Color(1, 1, 1, 0.5), 1.0)
+	
+	# Draw a small crosshair (plus sign)
+	var length = 3.0
+	draw_line(target_pos - Vector2(length, 0), target_pos + Vector2(length, 0), Color(1, 0, 0, 0.7), 1.0)
+	draw_line(target_pos - Vector2(0, length), target_pos + Vector2(0, length), Color(1, 0, 0, 0.7), 1.0)
